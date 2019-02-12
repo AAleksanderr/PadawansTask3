8b29666d8c416566aba9302855c1d1655b49b1a8
AAleksanderr <indra1@tut.by>
2019-02-12 21:36:13 +0300

    Complele.

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace PadawansTask3
 {
@@ -6,8 +7,16 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (a == 0 && b == 0) throw new ArgumentException();
+            if (a == 0 || b == 0) return Math.Max(a,b);
+            while (true)
+            {
+                var rem = Math.Max(a, b) - Math.Min(a, b);
+                if (rem == 0) return a;
+                if (Math.Min(a, b) % rem == 0) return rem;
+                a = Math.Min(a, b);
+                b = rem;
+            }
         }
     }
 }

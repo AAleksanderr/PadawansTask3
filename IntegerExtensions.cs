652dd8b9226b72a5136dfa26ca95023bbe7b0cd6
AAleksanderr <indra1@tut.by>
2019-02-13 16:39:48 +0300

    Fix

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 
 namespace PadawansTask3
 {
@@ -7,11 +6,16 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
             if (a == 0 && b == 0) throw new ArgumentException();
-            if (a == 0 || b == 0) return Math.Max(a,b);
+            if (a == 0) return b;
+            if (b == 0) return a;
+            {
+            }
             while (true)
             {
-                var rem = Math.Max(a, b) - Math.Min(a, b);
+                var rem = Math.Max(a, b) % Math.Min(a, b);
                 if (rem == 0) return a;
                 if (Math.Min(a, b) % rem == 0) return rem;
                 a = Math.Min(a, b);
